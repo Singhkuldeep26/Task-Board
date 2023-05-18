1a364a3962b8c647ed623f5927e7244ee3dad38b
kuldeep singh <pssingh1145@gmail.com>
2023-05-18 17:14:59 +0530

    commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require('./images/task-board.png'),
+    appIcon: require('./images/favicon.png')
+  },
+  colors: {
+    secondary: '#292929'
+  }
+}
+
+export default assets
